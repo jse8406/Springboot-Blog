5cc10f59c620e8ea5613c8b8e54bc2a5ea08ae86
jse <73538772+jse8406@users.noreply.github.com>
2024-07-01 12:15:34 +0900

    ENV setting

---
+++ src/main/java/me/jse/blog/jselog/JselogApplication.java
@@ -0,0 +1,13 @@
+package me.jse.blog.jselog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JselogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JselogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/jse/blog/jselog/JselogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jse.blog.jselog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JselogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
